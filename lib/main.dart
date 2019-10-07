3a6e80022d7df8bcbe7a7cd43de6698b52d79486
Jonathan Mtz <rockerjony@gmail.com>
2019-10-07 14:02:06 -0500

    Fitness app with graph animations
    Example of a profile Activity

---
+++ lib/main.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:s01e03/graph_widget.dart';
+
+const backgroundColor = Color(0xff5E37DB);
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: Row(
+          children: <Widget>[
+            SizedBox(width: 32.0),
+            Icon(Icons.arrow_back_ios),
+          ],
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                color: Colors.green.withOpacity(0.3),
+              ),
+              child: MaterialButton(
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                    SizedBox(width: 12.0),
+                    Text('Friends',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.green,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      backgroundColor: backgroundColor,
+      body: Column(
+        children: <Widget>[
+          userWidget(),
+          pointsWidget(),
+          GraphWidget(),
+          friendsWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget userWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32.0, top: 24.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 75.0,
+            height: 75.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              image: DecorationImage(
+                image: AssetImage('assets/jona.jpg'),
+              ),
+            ),
+          ),
+          SizedBox(width: 24.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Jonathan",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              Text("Martínez",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w400
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget pointsWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32.0, top: 10.0, right: 16.0),
+      child: Container(
+        width: double.infinity,
+        height: 112.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: Colors.white,
+        ),
+        child: CustomPaint(
+          painter: PointsPainter(),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("957",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text("place",
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.deepPurpleAccent
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("8 500",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text("points",
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.deepPurpleAccent
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget friendsWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: Container(
+        height: 80.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.white
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text("Friends",
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                        image: AssetImage('assets/jona.jpg'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                        image: AssetImage('assets/jona.jpg'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                        image: AssetImage('assets/jona.jpg'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 150.0),
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.yellow
+                      ),
+                      child: Center(
+                          child: Text("12...")
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PointsPainter extends CustomPainter {
+  Paint _paint = Paint();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _paint.color = Colors.deepPurpleAccent.withOpacity(0.15);
+
+    var path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width / 2, 0);
+    path.lineTo(size.width / 2 - 40, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawPath(path, _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
